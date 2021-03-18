40f5d0d6bacc5b1e61036953f18dc56274652d5d
Carlos Maradiaga <carlos.maradiaga@hotmail.com>
2021-03-18 11:39:13 -0600

    Sección 5 : Vue CLI 4 Completa

---
+++ src/components/AccionSaldo.vue
@@ -0,0 +1,31 @@
+<template>
+        <!-- Esta forma es valida, pero No permite enviarle parámetros. Para enviar parametros hacer los métodos aqui
+        en el componente Hijo.
+        <button @click="this.$emit('accion')">{{ texto }} </button> 
+        -->
+     <!-- En este caso, el evento click del botón llama al método accion aqui en este componente, y es este método el que
+          finalemente emite el evento @accion en el componente padre, el cual ejecuta su método asociado.-->
+     <button @click="accion" :disabled="desactivar">{{ texto }} </button>
+
+</template>
+
+<script>
+export default {
+    props:  {
+            texto: String,
+            desactivar: {
+                type: Boolean,
+                default: false
+                }
+        },
+    methods: {
+        accion() {
+            this.$emit('accion')
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/Cuenta.vue
+++ src/components/Cuenta.vue
@@ -6,20 +6,44 @@
   <div v-for="(item, indice) in servicios" :key="indice">
       {{ indice + 1}}  {{ item }}
   </div>
-
+    <br>
+    <!-- @accion se captura en el componente Hijo en este caso AccionSaldo -->
+    <accion-saldo texto="Aumentar Saldo" @accion="aumentar" />
+    <AccionSaldo texto="Disminuir Saldo" @accion="disminuir" :desactivar="desactivar"/>
 
 </template>
 
 <script>
+import AccionSaldo from './AccionSaldo.vue'
+
 export default {
+  components: { 
+          AccionSaldo 
+      },
     data() {
         return {
             saldo: 1000,
             cuenta: 'Visa',
             estado: false,
-            servicios: ['giro', 'abono', 'transferencia']
+            servicios: ['giro', 'abono', 'transferencia'],
+            desactivar:false
         }
     },
+    methods: {
+        aumentar() {
+            this.saldo+= 100;
+            this.desactivar = false
+        },
+        disminuir() {
+            if(this.saldo === 0) {
+                this.desactivar = true
+                alert('Sin Saldo Disponible')
+                return
+            }
+                
+            this.saldo-= 100;
+        }
+    }
 }
 </script>
 
